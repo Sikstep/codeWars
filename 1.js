30c7209865e428c728a0367b715c10ffef367106
Sikstep <Sikstep1988@gmail.com>
2023-01-17 09:40:32 +0300

    1-4

--- 1.js
+++ 1.js
@@ -32,6 +32,30 @@ const arr3 = arr.reduce((acc, el) => acc + el, 0);
 console.log(arr3);
 
 // #3
-function DNAtoRNA(dna) {
-    // create a function which returns an RNA sequence from the given DNA sequence
-  }
+// function DNAtoRNA(dna) {
+//     let a,
+//         b;
+//     a = dna.indexOf('T');
+        
+
+//   }
+
+
+// let a = 'GCAT',
+//     b = 'ACTG',
+//     c = 'TCAG';
+// console.log(a.indexOf('T'));
+// console.log(b.indexOf('T'));
+// console.log(c.indexOf('T'));
+
+// #4 Even or Odd
+function evenOrOdd(number) {
+  return number % 2 === 0 ? `Even` : `Odd`;
+}
+
+console.log(evenOrOdd(7));
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(-4));
+console.log(evenOrOdd(-17));
+
+
