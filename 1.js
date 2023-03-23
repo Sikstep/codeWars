9f8a1f818904547f0cd82eb4d2135c7c1603bf46
Sikstep <Sikstep1988@gmail.com>
2023-03-23 13:13:38 +0300

    71

--- 1.js
+++ 1.js
@@ -1073,3 +1073,10 @@ function removeEveryOther(arr) {
 }
 
 console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"]));
+
+// #71
+function cockroachSpeed(s) {
+  return Math.floor((s * 100000) / 3600);
+}
+
+console.log(cockroachSpeed(1.08));
