3ce16a28dac4db893e831703d450d0ff6c3ce5a6
Sikstep <Sikstep1988@gmail.com>
2023-03-29 12:09:16 +0300

    85+best solution

--- 1.js
+++ 1.js
@@ -1339,3 +1339,10 @@ var humanYearsCatYearsDogYears = function (humanYears) {
 };
 
 console.log(humanYearsCatYearsDogYears(1));
+
+// best solution
+var humanYearsCatYearsDogYears = function (y) {
+  if (y == 1) return [1, 15, 15];
+  if (y == 2) return [2, 24, 24];
+  return [y, (y - 2) * 4 + 24, (y - 2) * 5 + 24];
+};
