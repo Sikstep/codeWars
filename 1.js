97e118c1f8a33c95a0269a60841119f7ecf28586
Sikstep <Sikstep1988@gmail.com>
2023-04-05 12:02:02 +0300

    99

--- 1.js
+++ 1.js
@@ -1524,3 +1524,18 @@ function sum(a) {
 }
 
 console.log(sum(1)(2)(5)(6));
+
+// #99
+
+function finalGrade(exam, projects) {
+  switch (true) {
+    case exam > 90 || projects > 10:
+      return 100;
+    case exam > 75 && projects >= 5:
+      return 90;
+    case exam > 50 && projects >= 2:
+      return 75;
+    default:
+      return 0;
+  }
+}
