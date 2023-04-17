2212a36079a661b10de7717ccec7b6ccf67dcd47
Sikstep <Sikstep1988@gmail.com>
2023-04-17 10:24:43 +0300

    114 with filter

--- 1.js
+++ 1.js
@@ -1726,3 +1726,13 @@ function well(x) {
     : "Fail!";
 }
 console.log(well(["bad", "bad", "bad"]));
+// with filter
+
+const well1 = (x) => {
+  const good_count = x.filter((x) => x == "good").length;
+  return good_count < 1
+    ? "Fail!"
+    : good_count < 3
+    ? "Publish!"
+    : "I smell a series!";
+};
