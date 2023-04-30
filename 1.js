565a5c23c3646070e5d0b5738eed01d9ff4043b3
Sikstep <Sikstep1988@gmail.com>
2023-05-01 02:05:52 +0300

    136

--- 1.js
+++ 1.js
@@ -1937,3 +1937,16 @@ function getChar(c) {
 }
 
 console.log(getChar(55));
+
+// 136
+
+function howManyDalmations(number) {
+  
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+  
+  return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number == 101 ? dogs[3] : dogs[2];
+  
+  }
+
+  console.log(howManyDalmations(101));
+
