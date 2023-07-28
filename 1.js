526e0a9640d6923eb92f32657d19ff05c7c194c4
Sikstep <Sikstep1988@gmail.com>
2023-07-28 13:34:56 +0300

    190

--- 1.js
+++ 1.js
@@ -2706,3 +2706,14 @@ function howManydays(month) {
   }
   return 31;
 }
+
+//190
+
+function defineSuit(card) {
+  if (card.includes("♣")) return "clubs";
+  if (card.includes("♦")) return "diamonds";
+  if (card.includes("♥")) return "hearts";
+  if (card.includes("♠")) return "spades";
+}
+
+console.log(defineSuit("Q♠"));
