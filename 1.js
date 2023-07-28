650b2fb55ed3e6999f935a74600f120a027e5505
Sikstep <Sikstep1988@gmail.com>
2023-07-28 14:11:49 +0300

    192

--- 1.js
+++ 1.js
@@ -2721,7 +2721,25 @@ console.log(defineSuit("Q♠"));
 //191 regular expression проверка на строку от 4 до 16 символов, только строчные буквы, и все цифры включая _
 
 function validateUsr(username) {
+  // - `^`        Start from the beginning of the string.
+  // - `[]`       Allow any character specified, including...
+  // - `a-z`      anything from a to z,
+  // - `0-9`      anything from 0 to 9,
+  // - `_`        and underscore.
+  // - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+  // - `$`        End the string right after specified amount of allowed characters is given.
+
   return /^[a-z0-9_]{4,16}$/.test(username);
 }
 
 console.log(/[a-z]/.test("asda"));
+
+//192
+
+function array(string) {
+  let arr = string.split(",");
+  let finalArr = string.split(",").slice(1, -1).join(" ");
+  return arr.length <= 2 ? null : finalArr;
+}
+
+console.log(array("1"));
