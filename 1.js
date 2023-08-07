bd31d9b1c040ea964e28fdb34d8e89a246bda658
Sikstep <Sikstep1988@gmail.com>
2023-08-07 12:01:35 +0300

    194 +bs

--- 1.js
+++ 1.js
@@ -2772,3 +2772,11 @@ function cookie(x) {
 }
 
 console.log(cookie("Ryan"));
+
+//bs
+
+function cookie(x) {
+  var t = typeof x;
+  var who = t == "string" ? "Zach" : t == "number" ? "Monica" : "the dog";
+  return `Who ate the last cookie? It was ${who}!`;
+}
