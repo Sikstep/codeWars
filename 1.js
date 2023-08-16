668f950d934069048e83d345270eddc5804fb438
Sikstep <Sikstep1988@gmail.com>
2023-08-16 11:03:16 +0300

    205

--- 1.js
+++ 1.js
@@ -2914,3 +2914,12 @@ const shortenToDate1 = (longDate) => longDate.split(",")[0];
 function toCsvText(array) {
   return array.join("\n");
 }
+
+console.log(
+  toCsvText([
+    [0, 1, 2, 3, 45],
+    [10, 11, 12, 13, 14],
+    [20, 21, 22, 23, 24],
+    [30, 31, 32, 33, 34],
+  ])
+);
