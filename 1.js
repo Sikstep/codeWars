b478882d6f4cce841695178ff68b8811f3cb6080
Sikstep <Sikstep1988@gmail.com>
2023-08-19 21:58:57 +0300

    208

--- 1.js
+++ 1.js
@@ -2938,3 +2938,24 @@ function getNumberFromString(s) {
 }
 
 console.log(getNumberFromString("1"));
+
+//208
+
+function calculateTip(amount, rating) {
+  switch (rating.toLowerCase()) {
+    case "terrible":
+      return 0;
+    case "poor":
+      return Math.ceil(amount * 0.05);
+    case "good":
+      return Math.ceil(amount * 0.1);
+    case "great":
+      return Math.ceil(amount * 0.15);
+    case "excellent":
+      return Math.ceil(amount * 0.2);
+    default:
+      return "Rating not recognised";
+  }
+}
+
+console.log(calculateTip(20, "Excellent"));
