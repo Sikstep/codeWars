27b3e00b9dcbc6678c66e30cc456066313630308
Sikstep <Sikstep1988@gmail.com>
2023-09-10 20:57:49 +0300

    229

--- 1.js
+++ 1.js
@@ -3244,6 +3244,7 @@ function getStatus(isBusy) {
 
 console.log(getStatus(false).status);
 
+//229
 function pointsPer48(ppg, mpg) {
   return ppg === 0 || mpg === 0 ? 0 : Number(((ppg / mpg) * 48).toFixed(1));
 }
