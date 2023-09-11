e6a8337b569b584530590a3ec48fb2d6df9f13e3
Sikstep <Sikstep1988@gmail.com>
2023-09-11 17:18:25 +0300

    231+bs

--- 1.js
+++ 1.js
@@ -3296,3 +3296,10 @@ function isPythagoreanTriple(integers) {
 }
 
 console.log(isPythagoreanTriple([3, 4, 5]));
+
+//bs
+
+function isPythagoreanTriple1(integers) {
+  let massiv = integers.sort((a, b) => a - b);
+  return massiv[0] ** 2 + massiv[1] ** 2 == massiv[2] ** 2;
+}
