6950b677c8c781261e15fc6c0239dfef414f9daa
Sikstep <Sikstep1988@gmail.com>
2023-09-30 01:55:42 +0300

    240 completed + bs regular - completed

--- 1.js
+++ 1.js
@@ -3552,3 +3552,17 @@ function correctPolishLetters(string) {
 }
 
 // solution with regular expression
+function correctPolishLetters(string) {
+  var dict = {
+    ą: "a",
+    ć: "c",
+    ę: "e",
+    ł: "l",
+    ń: "n",
+    ó: "o",
+    ś: "s",
+    ź: "z",
+    ż: "z",
+  };
+  return string.replace(/[ąćęłńóśźż]/g, (match) => dict[match]);
+}
