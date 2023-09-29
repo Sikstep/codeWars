bb70d472162b3f38e8d96d0a771bb4ff040c8207
Sikstep <Sikstep1988@gmail.com>
2023-09-30 01:53:58 +0300

    240 completed + bs with my result

--- 1.js
+++ 1.js
@@ -3529,3 +3529,24 @@ function correctPolishLetters(string) {
 }
 
 console.log(correctPolishLetters("Jędrzej Błądziński"));
+
+//bs the same solution
+
+var polishLetters = {
+  ą: "a",
+  ć: "c",
+  ę: "e",
+  ł: "l",
+  ń: "n",
+  ó: "o",
+  ś: "s",
+  ź: "z",
+  ż: "z",
+};
+
+function correctPolishLetters(string) {
+  return string
+    .split("")
+    .map((c) => polishLetters[c] || c)
+    .join("");
+}
