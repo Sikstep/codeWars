5ff3e41d4aa2169d674f1d01c8910842ef8576b1
Sikstep <Sikstep1988@gmail.com>
2024-01-03 18:02:58 +0300

    263 immutable + comblete

--- 1.js
+++ 1.js
@@ -4165,3 +4165,24 @@ function ifChuckSaysSo() {
 console.log(ifChuckSaysSo());
 
 //263 imutable
+
+const startState = {
+  todolistId1: [
+    { id: "1", title: "CSS", isDone: false },
+    { id: "2", title: "JS", isDone: true },
+    { id: "3", title: "React", isDone: false },
+  ],
+  todolistId2: [
+    { id: "1", title: "bread", isDone: false },
+    { id: "2", title: "milk", isDone: true },
+    { id: "3", title: "tea", isDone: false },
+  ],
+};
+
+const newTask = { id: "4", title: "coffee", isDone: false };
+
+const newState = {
+  ...startState,
+  ["todolistId2"]: [...startState["todolistId2"], newTask],
+};
+console.log(newState);
